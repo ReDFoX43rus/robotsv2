5e6137b9fc0db2d2f8f84c01e8d82a50642a59c5
ReDFoX43rus <red-fox_ddrace@mail.ru>
2018-02-28 07:56:19 +0300

    wifi scan implemented

--- src/cconsole/ccmds/cmd_wifi.cpp
+++ src/cconsole/ccmds/cmd_wifi.cpp
@@ -1,7 +1,34 @@
 #include "uart.h"
-
 #include "wifi/wifi.h"
+#include "string.h"
+
+static CWifi wifi;
+
 void CmdWifiHandler(int argc, char **argv){
-	int result = wifi_init_sta("DIR-300A", "12169931");
-	uart << "wifi_init: " << result << endl;
+	// int res = wifi.Init();
+	// uart << "wifi inited: " << res << endl;
+    //
+	// for(int i = 1; i <= 13; i++){
+	// 	res = wifi.Search(i);
+	// 	if(!res)
+	// 		wifi.ListScanRecords();
+	// }
+
+	if(argc < 2){
+		uart << "Usage: wifi [cmd]" << endl;
+	}
+
+	if(!strcmp(argv[1], "init"))
+		uart << "wifi init: " << wifi.Init() << endl;
+	else if(!strcmp(argv[1], "scan")){
+		int res;
+		for(int i = 1; i <= 13; i++){
+			res = wifi.Search(i);
+			if(!res)
+				wifi.ListScanRecords();
+		}
+	}
+	else if(!strcmp(argv[1], "connect")){
+		uart << "wifi connect: " << wifi.Connect(argv[2], argv[3]);
+	}
 }
--- src/wifi/wifi.h
+++ src/wifi/wifi.h
@@ -1,14 +1,31 @@
 #ifndef WIFI_WIFI_H
 #define WIFI_WIFI_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "freertos/event_groups.h"
 
-int wifi_init_sta(const char *ssid, const char *pwd);
+class CWifi {
 
-#ifdef __cplusplus
-}
-#endif
+public:
+	static EventGroupHandle_t m_EventGroup;
+	static esp_err_t event_handler(void *ctx, system_event_t *event);
 
+	CWifi();
+	~CWifi();
+
+	int Init();
+	int Search(uint8_t channel);
+	void ListScanRecords();
+	int Connect(char *ssid, char *pwd);
+	//int wifi_init_sta(const char *ssid, const char *pwd);
+private:
+	bool m_Inited;
+	wifi_ap_record_t *m_ScanRecords;
+	uint16_t m_CurrentScanRecords;
+
+	enum{
+		WIFI_CONNECTED_BIT=BIT0
+	};
+};
 #endif /* end of include guard: WIFI_WIFI_H */
