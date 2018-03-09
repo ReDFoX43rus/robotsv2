da38317feedd00594c78012f292ac9a331a7e42c
ReDFoX43rus <red-fox_ddrace@mail.ru>
2018-03-09 23:19:06 +0300

    main.cpp cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,7 @@
 #include "i2c-ths.h"
 #include "cconsole/console.h"
 
-#include "drivers/i2c-lcd1602.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#include "drivers/lcd_driver.h"
-
 extern "C" void initialize_filesystem(void);
-
-void test_lcd_driver()
-{
-	CLcdDriver lcd(0x27, true);
-	lcd.Init();
-
-	uart << "cursor on" << endl;
-	lcd.Display(true, true, false);
-
-	lcd.WriteString("Hello, Faramoz!");
-	lcd.MoveCursor(1, 2);
-	lcd.WriteString("Hello, Egor!");
-}
 
 extern "C" void app_main()
 {
@@ -35,8 +15,6 @@ extern "C" void app_main()
 	console.HandleCmd(uart, "wifi init");
 	console.HandleCmd(uart, "wifi connect Faramoz 12169931");
 	console.HandleCmd(uart, "tcp 0");
-
-	test_lcd_driver();
 
 	while(1)
 		console.WaitForCmd(uart);
